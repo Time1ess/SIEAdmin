ff7d7826cfe94bb43be908d086446f0af435aaad
David <youchen.du@gmail.com>
2018-05-01 03:02:03 +0800

    [fix] 使用绝对路径读取配置文件

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
+import os.path as osp
 import configparser
 
 
 config = configparser.ConfigParser(interpolation=None)
-config.read('SIEAdmin.ini')
+config.read(osp.join(osp.abspath(osp.dirname(__file__)), 'SIEAdmin.ini'))
